37d1d9bd0adc24cd486dba758ac2310bada0cd2e
Malcolm Hall <malhal@users.noreply.github.com>
2018-02-19 13:01:03 +0000

    Made KVOController public

--- MHFoundation/MHFoundation.h
+++ MHFoundation/MHFoundation.h
@@ -19,6 +19,7 @@
 #import <MHFoundation/MHFURLSessionTaskOperation.h>
 #import <MHFoundation/MHFRESTOperation.h>
 #import <MHFoundation/MHFBatchRESTOperation.h>
+#import <MHFoundation/MHFKVOController.h>
 
 #import <MHFoundation/NSDictionary+MHF.h>
 #import <MHFoundation/NSSet+MHF.h>
